dfbade253aa27b41ab4212c92127a0148981577e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-04-05 16:48:46 +0700

    chore: format

--- src/main.rs
+++ src/main.rs
@@ -11,46 +11,46 @@ mod tests;
 
 #[async_std::main]
 async fn main() -> Result<(), Error> {
-    dotenv().ok();
-    pretty_env_logger::init();
-    let app = server().await?;
-    app.listen("127.0.0.1:5000").await?;
-    Ok(())
+	dotenv().ok();
+	pretty_env_logger::init();
+	let app = server().await?;
+	app.listen("127.0.0.1:5000").await?;
+	Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
 enum Error {
-    #[error(transparent)]
-    DbError(#[from] sqlx::Error),
+	#[error(transparent)]
+	DbError(#[from] sqlx::Error),
 
-    #[error(transparent)]
-    IoError(#[from] std::io::Error),
+	#[error(transparent)]
+	IoError(#[from] std::io::Error),
 
-    #[error(transparent)]
-    EnvVarError(#[from] std::env::VarError),
+	#[error(transparent)]
+	EnvVarError(#[from] std::env::VarError),
 }
 
 /// description
 async fn server() -> Result<Server<State>, Error> {
-    let db_url = std::env::var("DATABASE_URL")?;
-    let pool = Pool::<Postgres>::connect(&db_url).await?;
+	let db_url = std::env::var("DATABASE_URL")?;
+	let pool = Pool::<Postgres>::connect(&db_url).await?;
 
-    let mut app: Server<State> = Server::with_state(State { db_pool: pool });
-    app.at("/").get(root_endpoint);
+	let mut app: Server<State> = Server::with_state(State { db_pool: pool });
+	app.at("/").get(root_endpoint);
 
-    Ok(app)
+	Ok(app)
 }
 
 /// Root endpoint
 async fn root_endpoint(req: Request<State>) -> tide::Result<Value> {
-    let state = &req.state().db_pool;
-    let rows = query!("select 1 as one").fetch_one(state).await?;
-    dbg!(rows);
-    let js = json!({"message": "server is running"});
-    Ok(js)
+	let state = &req.state().db_pool;
+	let rows = query!("select 1 as one").fetch_one(state).await?;
+	dbg!(rows);
+	let js = json!({"message": "server is running"});
+	Ok(js)
 }
 
 #[derive(Debug, Clone)]
 struct State {
-    db_pool: Pool<Postgres>,
+	db_pool: Pool<Postgres>,
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -6,14 +6,14 @@ use tide_testing::TideTestingExt;
 
 #[async_std::test]
 async fn test_endpoint() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
-    let mut server = server().await?;
-    server.at("/").get(root_endpoint);
-    let raw = server.get("/").recv_string().await?;
-    let got: Value = serde_json::from_str(&raw)?;
-    let expected = json!({"message": "server is running"});
+	dotenv().ok();
+	let mut server = server().await?;
+	server.at("/").get(root_endpoint);
+	let raw = server.get("/").recv_string().await?;
+	let got: Value = serde_json::from_str(&raw)?;
+	let expected = json!({"message": "server is running"});
 
-    assert_json_include!(actual: got, expected: expected);
+	assert_json_include!(actual: got, expected: expected);
 
-    Ok(())
+	Ok(())
 }
