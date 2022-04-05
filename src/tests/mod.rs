d179a6873cb31721a100fd4d018c0edcccf9b7cc
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-04-05 16:59:11 +0700

    test: refactor to use recv_json

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -4,13 +4,14 @@ use dotenv::dotenv;
 use serde_json::{json, Value};
 use tide_testing::TideTestingExt;
 
+mod test_helpers;
+
 #[async_std::test]
-async fn test_endpoint() -> Result<(), Box<dyn std::error::Error>> {
+async fn test_root() -> Result<(), Box<dyn std::error::Error>> {
 	dotenv().ok();
 	let mut server = server().await?;
 	server.at("/").get(root_endpoint);
-	let raw = server.get("/").recv_string().await?;
-	let got: Value = serde_json::from_str(&raw)?;
+	let got: Value = server.get("/").recv_json().await?;
 	let expected = json!({"message": "server is running"});
 
 	assert_json_include!(actual: got, expected: expected);
