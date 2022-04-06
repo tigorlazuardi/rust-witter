e22c653ab4f21433d17773d9498a6c52249b1f04
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-04-06 16:32:05 +0700

    test: fix test error

--- src/tests/endpoints/user.rs
+++ src/tests/endpoints/user.rs
@@ -1,16 +1,16 @@
-use assert_json_diff::assert_json_include;
+use assert_json_diff::{assert_json_eq, assert_json_include};
 use tide::StatusCode;
 
 use crate::tests::test_helpers::*;
 
-// #[async_std::test]
-// async fn list_users() {
-// 	let server = test_setup().await;
-// 	let mut resp = server.get("/users").send().await.unwrap();
-// 	assert_eq!(resp.status(), StatusCode::Ok);
-// 	let got: Vec<Value> = resp.body_json().await.unwrap();
-// 	assert_json_eq!(got, json!([]));
-// }
+#[async_std::test]
+async fn list_users() {
+	let server = test_setup().await;
+	let mut resp = server.get("/users").send().await.unwrap();
+	assert_eq!(resp.status(), StatusCode::Ok);
+	let got: Vec<Value> = resp.body_json().await.unwrap();
+	assert_json_eq!(got, json!([]));
+}
 
 #[async_std::test]
 async fn create_user() {
--- src/tests/test_helpers.rs
+++ src/tests/test_helpers.rs
@@ -2,12 +2,88 @@ use crate::{make_pg_pool, server, State};
 pub use assert_json_diff::assert_json_include;
 pub use dotenv::dotenv;
 pub use serde_json::{json, Value};
+use sqlx::{Connection, PgConnection, Postgres};
 pub use tide::Server;
 pub use tide_testing::TideTestingExt;
 
 /// Setup testing
 pub async fn test_setup() -> Server<State> {
 	dotenv().ok();
+	let var = "DATABASE_URL_TEST";
+	let env_var = std::env::var(var).unwrap();
+	drop_db(&env_var).await;
+	exec_migrations(&env_var).await;
 	let pool = make_pg_pool("DATABASE_URL_TEST").await.unwrap();
 	server(pool)
+}
+
+pub async fn drop_db(db_url: &str) {
+	let (db_name, pg_conn) = parse_db_url(db_url);
+
+	let mut conn = PgConnection::connect(pg_conn).await.unwrap();
+
+	let query = format!(
+		r#"
+		SELECT pg_terminate_backend(pg_stat_activity.pid)
+		FROM pg_stat_activity
+		WHERE pg_stat_activity.datname = '{db}'
+		AND pid <> pg_backend_pid();
+		"#,
+		db = db_name
+	);
+
+	sqlx::query::<Postgres>(&query)
+		.execute(&mut conn)
+		.await
+		.unwrap();
+
+	let query = format!(
+		r#"
+        DROP DATABASE IF EXISTS "{db}";
+        "#,
+		db = db_name
+	);
+
+	sqlx::query::<Postgres>(&query)
+		.execute(&mut conn)
+		.await
+		.unwrap();
+}
+
+pub async fn exec_migrations(db_url: &str) {
+	let (db_name, pg_con) = parse_db_url(db_url);
+
+	let mut conn = PgConnection::connect(pg_con).await.unwrap();
+
+	let query = format!(
+		r#"
+		CREATE DATABASE "{db}"
+		"#,
+		db = db_name
+	);
+
+	sqlx::query::<Postgres>(&query)
+		.execute(&mut conn)
+		.await
+		.unwrap();
+
+	drop(conn);
+	let mut conn = PgConnection::connect(db_url).await.unwrap();
+
+	let query = async_std::fs::read_to_string("setup/setup.sql")
+		.await
+		.unwrap();
+
+	sqlx::query::<Postgres>(&query)
+		.execute(&mut conn)
+		.await
+		.unwrap();
+}
+
+pub fn parse_db_url(db_url: &str) -> (&str, &str) {
+	let separator_pos = db_url.rfind('/').unwrap();
+	let pg_conn = &db_url[separator_pos + 1..];
+	let db_name = &db_url[..separator_pos];
+
+	(pg_conn, db_name)
 }
